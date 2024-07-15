f8853bb5068846231526f619c6ddb5de70db1e54
Ravella <89604314+dev1287@users.noreply.github.com>
2024-07-15 13:19:22 -0500

    Update firshell.sh

--- shell/firshell.sh
+++ shell/firshell.sh
@@ -1,1 +1,13 @@
+person1=ramesh
+person2=raghu
 
+echo "how are you $person2 "
+echo "i am good , how are you $person1"
+
+echo "-------------------------------------"
+USERNAME=$1
+PASSWORD=$2
+echo "please enter USERNAME::"
+read -s USERNAME
+echo "please enter PASSWORD::"
+read -s USERNAME
